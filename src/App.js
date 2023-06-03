b9a6da8ca4a91b5c33cba26a55c9d5d7adf652af
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-06-03 03:07:10 +0100

    Modify component to contain imported components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Header from './components/Header';
+import Footer from './components/Footer';
+import Likes from './Pages/Likes';
+import Messages from './Pages/Messages';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets build Tinder clone</h1>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/explore" element={<Likes />} />
+          <Route path="/likes" element={<Messages />} />
+          <Route path="/matches" element={<Messages />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
